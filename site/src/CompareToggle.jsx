c082d7372c79dbd1c55e2fdaf562c6ffa2a28307
Benjamin Clark <clarkben@meta.com>
2025-04-02 09:08:53 -0400

    QA Tiles now have a separate version selector

--- site/src/CompareToggle.jsx
+++ site/src/CompareToggle.jsx
@@ -2,25 +2,15 @@ import { useState, useMemo, useCallback } from "react";
 import CompareIcon from "@mui/icons-material/Compare";
 import PropTypes from "prop-types";
 import "./CompareToggle.css";
-
-// TODO: Get from manifest once the generation is finalized!
-export const VERSION_OPTIONS = [
-  "2025-03-19.1",
-  "2025-02-19.0",
-  "2025-01-22.0",
-  "2024-12-18.0",
-  "2024-11-13.0",
-  "2024-10-23.0",
-  "2024-09-18.0",
-  "2024-08-20.0",
-  "2024-07-22.0",
-];
+import { VERSION_OPTIONS, TWO_WEEKS_DS } from "./VersionConstants";
 
 export function useComparisonState() {
   const [compareMode, setCompareMode] = useState(false);
   const [activeMap, setActiveMap] = useState("left");
   const [leftVersion, setLeftVersion] = useState(VERSION_OPTIONS[0]);
   const [rightVersion, setRightVersion] = useState(VERSION_OPTIONS[0]);
+  const [leftQaVersion, setLeftQaVersion] = useState(TWO_WEEKS_DS[1]);
+  const [rightQaVersion, setRightQaVersion] = useState(TWO_WEEKS_DS[0]);
 
   const leftMapStyle = useMemo(
     () => ({
@@ -53,6 +43,10 @@ export function useComparisonState() {
     setLeftVersion,
     rightVersion,
     setRightVersion,
+    leftQaVersion,
+    setLeftQaVersion,
+    rightQaVersion,
+    setRightQaVersion,
   };
 }
 
