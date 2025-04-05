2fd8e22c3e4e7a66562562faa37c64f9a627fd61
Benjamin Clark <clarkben@meta.com>
2025-04-05 13:43:19 -0400

    Fix #201 by adding sticky headers.

--- site/src/qa_info_bar/QaDataRenderer.jsx
+++ site/src/qa_info_bar/QaDataRenderer.jsx
@@ -9,21 +9,19 @@ export default function QaDataRenderer({fileParsed, jsonContents, viewState, set
       const rowData = jsonContents.slice(1);
       return (    
         <>
-          <div className="qadata-container header">
-            <div className="qadata-header">
+          <tr className="qadata-container ">
+            <tr className="qadata-header">
               {headers.map(header => 
-                <div key={header} className="qadata-header-item"> 
+                <th key={header} className="qadata-header-item"> 
                   {header}
-                </div>
+                </th>
               )}
-            </div>
-          </div>
-          <div className="qadata-container row">
+            </tr>
             { rowData ? 
               <QaRowRenderer headers={headers} rows={rowData} viewState={viewState} setViewState={setViewState}></QaRowRenderer> : 
               <></>
             }
-          </div>
+          </tr>
         </>
       );
     } else {
@@ -61,32 +59,32 @@ function QaRowRenderer({headers, rows, viewState, setViewState}){
 
     rows.map((row, i) => {
       return(
-        <div key={i + row[2]} className="qadata-rows">
+        <tr key={i + row[2]} className="qadata-rows">
         {
           row.map((rowItem, j) => {
             if (j === locIndex) {
               return (
-                <div 
+                <td 
                   key={rowItem} 
                   className="qadata-row-item" 
                 >
                   <a onClick={() => setLocation(rowItem)}>jump to location</a>
-                </div>
+                </td>
               )
             }else {
               return (
-                <div 
+                <td 
                   key={rowItem} 
                   className="qadata-row-item" 
                 >
                   {rowItem}
-                </div>
+                </td>
               )
             }
           }
           )
         }
-        </div>  
+        </tr>  
       )
     })
   )
